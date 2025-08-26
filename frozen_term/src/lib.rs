a7d62dd4e70a1dfee34782d24902ce8a6048ba6b
L. Wlcek <luca@it-rahn.de>
2025-08-26 17:06:54 +0200

    rename component files

--- frozen_term/src/lib.rs
+++ frozen_term/src/lib.rs
@@ -1,12 +1,9 @@
 mod terminal;
-mod terminal2;
 mod terminal_grid;
 mod wezterm;
 
 #[cfg(feature = "local-terminal")]
 pub mod local_terminal;
-#[cfg(feature = "local-terminal")]
-pub mod local_terminal2;
 
-pub use terminal::{Action, Message, Terminal, TerminalSize, style::Style};
+pub use terminal::{Action, Message, Terminal, style::Style};
 pub use terminal_grid::Size;
